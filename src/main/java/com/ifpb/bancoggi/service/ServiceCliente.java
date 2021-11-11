24eb3ddacea1234c50a4b55ccf54476c531e3e74
GeovanaStefani <geovanastefani2011@gmail.com>
2021-11-11 19:19:48 -0300

    Excluindo a coluna senhaConfirmada do banco

--- src/main/java/com/ifpb/bancoggi/service/ServiceCliente.java
+++ src/main/java/com/ifpb/bancoggi/service/ServiceCliente.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.math.BigInteger;
 import java.security.MessageDigest;
