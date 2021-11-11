c79426a785cafaa9afe53e92a1a7709231f7c866
ikoliveira <igor.kadson@academico.ifpb.edu.br>
2021-11-11 16:34:45 -0300

    ajusta Application to Heroku

--- src/main/java/com/ifpb/bancoggi/BancoggiApplication.java
+++ src/main/java/com/ifpb/bancoggi/BancoggiApplication.java
@@ -1,13 +1,21 @@
 package com.ifpb.bancoggi;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class BancoggiApplication {
 
+	@Value("${PORT}")
+	private static String appPort;
+
 	public static void main(String[] args) {
-		SpringApplication.run(BancoggiApplication.class, args);
+		SpringApplication app = new SpringApplication(BancoggiApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", appPort));
+		app.run(args);
 	}
 
 }
